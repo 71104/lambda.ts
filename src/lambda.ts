a026cb50c4ff22246951bac22fff580af5d8d9e8
Alberto La Rocca <a71104@gmail.com>
2023-10-20 16:48:10 +0200

    Close the type before returning it.

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,6 @@
 import { NodeInterface } from './ast.js';
 import { Parser } from './parser.js';
-import { EMPTY_TYPE_CONTEXT, TauType } from './types.js';
+import { EMPTY_TYPE_CONTEXT, TypeScheme } from './types.js';
 import { EMPTY_VALUE_CONTEXT, ValueInterface } from './values.js';
 
 export function parse(input: string): NodeInterface {
@@ -10,11 +10,11 @@ export function parse(input: string): NodeInterface {
   return ast;
 }
 
-export function evaluate(input: string): [TauType, ValueInterface] {
+export function evaluate(input: string): [TypeScheme, ValueInterface] {
   const parser = new Parser(input);
   const ast = parser.parse();
   parser.resetVariableGenerator();
   const { substitution, type } = ast.getType(EMPTY_TYPE_CONTEXT);
   const value = ast.evaluate(EMPTY_VALUE_CONTEXT);
-  return [type.substitute(substitution), value];
+  return [type.substitute(substitution).close(), value];
 }
